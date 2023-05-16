0f5588797a1d9fccf0eeef8c64103daae97e176d
pjn51 <58340957+pjn51@users.noreply.github.com>
2023-05-16 10:12:35 -0700

    Calculator

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 document.body.innerHTML = "This is some content";
+
+function addNumbers() {
+    var first = parseFloat(document.getElementById("variableOne").value);
+    var second = parseFloat(document.getElementById("variableTwo").value);
+
+    if (isNaN(first) || isNaN(second)) {
+        document.getElementById("result").textContent = "Both inputs must be numbers.";
+    } else {
+        var result = first + second;
+        document.getElementById("result").textContent = first + " + " + second + " = " + result;
+    }
+}
