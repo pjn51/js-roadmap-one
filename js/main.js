2cdf10360137ed7765f7df494d264bede921b310
pjn51 <58340957+pjn51@users.noreply.github.com>
2023-05-16 10:36:30 -0700

    division feature, css

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.body.innerHTML = "This is some content";
+// main.js
 
 function addNumbers() {
     var first = parseFloat(document.getElementById("variableOne").value);
@@ -9,5 +9,21 @@ function addNumbers() {
     } else {
         var result = first + second;
         document.getElementById("result").textContent = first + " + " + second + " = " + result;
+    }
+}
+
+function divideNumbers() {
+    var first = parseFloat(document.getElementById("variableOne").value);
+    var second = parseFloat(document.getElementById("variableTwo").value);
+
+    if (isNaN(first) || isNaN(second)) {
+        document.getElementById("result").textContent = "Both inputs must be numbers.";
+    } else {
+        if (second == 0) {
+            document.getElementById("result").textContent = "Cannot divide by zero.";
+        } else {
+            var result = first / second;
+            document.getElementById("result").textContent = first + " / " + second + " = " + result;
+        }   
     }
 }
